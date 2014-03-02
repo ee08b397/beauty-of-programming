4248a082e104c3880954103e4a01382a52cef125
Zhu Li <zhuli19901106@gmail.com>
2014-03-02 12:43:29 +0800

    add bop files
    201403021243

---
+++ bop1.2-chinese-chess.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+/*
+	012
+	345
+	678
+*/
+int main()
+{
+	int n = 81;
+	while (n--) {
+		if (n / 9 % 3 == n % 9 % 3) {
+			printf("(%d, %d)\n", n / 9 + 1, n % 9 + 1);
+		}
+	}
+	
+	return 0;
+}
